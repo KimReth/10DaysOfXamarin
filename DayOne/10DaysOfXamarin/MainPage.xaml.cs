44a0c291ea5007239b5b9001bf59f57d082548ca
LAPTOP-L8E5G43E\Nikki <kimberlyretherford@icloud.com>
2021-03-02 19:31:31 -0500

    Add project files.

---
+++ DayOne/10DaysOfXamarin/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _10DaysOfXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(nameEntry.Text))
+            {
+                greetingLabel.Text = $"Hello {nameEntry.Text}, welcome to 10 days of Xamarin!";
+            }
+            else
+            {
+                DisplayAlert("Error", "Your name can't be empty", "Oh right");
+            }
+        }
+    }
+
+
+}
