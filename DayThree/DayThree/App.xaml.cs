eb660545169a489fc30762e6d3b85fec41e819ef
LAPTOP-L8E5G43E\Nikki <kimberlyretherford@icloud.com>
2021-03-07 18:02:29 -0500

    Day 3 completed, added a SQLite DB for both Android and iOS

---
+++ DayThree/DayThree/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DayThree
+{
+    public partial class App : Application
+    {
+        public static string DatabasePath;
+        public App(string databasePath)
+        {
+            InitializeComponent();
+            DatabasePath = databasePath;
+            MainPage = new MainPage();
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
