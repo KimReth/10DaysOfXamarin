b9ac370df43acdbfc68126b2d4f172a28bede7d3
LAPTOP-L8E5G43E\Nikki <kimberlyretherford@icloud.com>
2021-03-08 15:52:06 -0500

    Day 4 completed

---
+++ DayFour/TenDaysOfXamarin/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using TenDaysOfXamarin.Models;
+using SQLite;
+
+namespace TenDaysOfXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckIfShouldBeEnabled()
+        {
+            saveButton.IsEnabled = false;
+            if (!string.IsNullOrWhiteSpace(titleEntry.Text) && !string.IsNullOrWhiteSpace(contentEditor.Text))
+                saveButton.IsEnabled = true;
+        }
+
+        void TitleEntry_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            CheckIfShouldBeEnabled();
+        }
+
+        void ContentEditor_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            CheckIfShouldBeEnabled();
+        }
+
+        void SaveButton_Clicked(object sender, System.EventArgs e)
+        {
+            Experience newExperience = new Experience()
+            {
+                Title = titleEntry.Text,
+                Content = contentEditor.Text,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+
+            int insertedItems = 0;
+            //Everytime a new connection is created, it must be closed. Using statement helps ensure this.
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath)) 
+            {
+                conn.CreateTable<Experience>();
+                conn.Insert(newExperience);
+            }
+
+            if (insertedItems > 0)
+            {
+                titleEntry.Text = string.Empty;
+                contentEditor.Text = string.Empty;
+            }
+            else
+            {
+                DisplayAlert("Error", "There was an error inserting the Experience, please try again", "Ok");
+            }
+        }
+
+        void ContentEntry_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DayFour/TenDaysOfXamarin/Models/Experience.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace TenDaysOfXamarin.Models
+{
+    //This is the class that models the table. Name of class is name of table.
+    public class Experience
+    {
+        [PrimaryKey, AutoIncrement] // using directive for SQLite needed
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
