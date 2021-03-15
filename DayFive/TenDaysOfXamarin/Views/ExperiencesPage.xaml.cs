486089a219033910da75af994c82f3f0c8b5fc11
LAPTOP-L8E5G43E\Nikki <kimberlyretherford@icloud.com>
2021-03-14 20:54:03 -0400

    Completed Day 5

---
+++ DayFive/TenDaysOfXamarin/Views/ExperiencesPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TenDaysOfXamarin.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TenDaysOfXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExperiencesPage : ContentPage
+    {
+        public ExperiencesPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked (object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ReadExperiences();
+        }
+        private void ReadExperiences()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+            {
+                conn.CreateTable<Experience>();
+                List<Experience> experiences = conn.Table<Experience>().ToList();
+                experiencesListView.ItemsSource = experiences;
+            }
+        }
+    }
+}
