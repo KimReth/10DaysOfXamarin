e90a9eeb2858c3a1deb0aa1e28b7149388486600
LAPTOP-L8E5G43E\Nikki <kimberlyretherford@icloud.com>
2021-03-18 20:52:13 -0400

    Day 6 completed, added geolocation

---
+++ DaySix/TenDaysOfXamarin/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using Plugin.Geolocator;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Geolocator.Abstractions;
+
+namespace TenDaysOfXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            locator.PositionChanged += Locator_PositionChanged;
+        }
+
+        [Obsolete]
+        private async void GetLocationPermission()
+        {
+            var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.LocationWhenInUse);
+            if (status != PermissionStatus.Granted)
+            {
+                if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.LocationWhenInUse))
+                {
+                    // This is not the actual permission request
+                    await DisplayAlert("Need your permission", "We need to access your location", "Ok");
+                }
+                // This is the actual permission request
+                var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.LocationWhenInUse);
+                if (results.ContainsKey(Permission.LocationWhenInUse))
+                    status = results[Permission.LocationWhenInUse];
+            }
+            if (status == PermissionStatus.Granted)
+            {
+                // Granted! Get the location
+            }
+            else
+            {
+                await DisplayAlert("Access to location denied", "We don't have access to your location", "Ok");
+            }
+
+        }
+
+        IGeolocator locator = CrossGeolocator.Current;
+        Position position;
+        private async void GetLocation()
+        {
+            position = await locator.GetPositionAsync();
+            await locator.StartListeningAsync(TimeSpan.FromMinutes(30), 500);
+        }
+
+        void Locator_PositionChanged(object sender, PositionEventArgs e)
+        {
+            position = e.Position; // this uses the global variable defined earlier
+        }
+
+        [Obsolete]
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            GetLocationPermission();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            locator.StopListeningAsync();
+        }
+    }
+
+}
